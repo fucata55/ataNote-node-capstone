5b128deee28058f88bd6f56c869ebdbf64a23378
fucata <sliong325@gmail.com>
2017-10-25 20:35:20 -0500

    added test

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var express = require('express');
 var app = express();
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.promise;
+
+//const {PORT, DATABASE_URL} = require('./config')
+
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8083);
+
+exports.app = app
---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const should = chai.should();
+
+const {
+    app
+} = require('../server');
+const express = require('express');
+
+describe('index page', function () {
+    it('should show "Hello world" and status 200', function (done) {
+        chai.request(app).get('/').end(function (err, res) {
+            res.should.have.status(200);
+            res.should.be.html;
+            done();
+        });
+    });
+});
