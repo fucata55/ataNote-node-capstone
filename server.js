b646870ad832170ee5e4e58c322dbfcd15727aa8
fucata <sliong325@gmail.com>
2017-11-05 14:11:09 -0600

    Testing connection

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.promise;
 //const {PORT, DATABASE_URL} = require('./config')
 
 
-app.use(express.static('public/pages/landing-page'));
+app.use(express.static('../public/pages/landing-page'));
 app.listen(process.env.PORT || 8081);
 
 exports.app = app
