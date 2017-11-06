9c2868453d415bede1f1e61d4d537cafd5fff6fa
fucata <sliong325@gmail.com>
2017-11-05 20:03:08 -0600

    Linked server side JS

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ mongoose.Promise = global.promise;
 
 //const {PORT, DATABASE_URL} = require('./config')
 
+//route urls
+const registerRouter = require('./routes/registerRouter');
+const loginRouter = require('./routes/loginRouter');
+const editorRouter = require('./routes/editorRouter');
+const homeRouter = require('./routes/homeRouter');
+const profileRouter = require('./routes/profileRouter')
 
 app.use(express.static('public/pages'));
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/home', homeRouter);
+app.use('/profile', profileRouter);
+app.use('/register', registerRouter);
+
+
 app.listen(process.env.PORT || 8081);
 
 exports.app = app
