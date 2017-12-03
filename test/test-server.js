d3efa53df0a985c56d9c00a06b445eb255237086
fucata <sliong325@gmail.com>
2017-12-03 16:37:18 -0600

    Builed test setup and GET users

--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,93 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
-
 const should = chai.should();
+const faker = require('faker');
+const mongoose = require('mongoose');
 
 const {
-    app
+    app,
+    runServer,
+    closeServer
 } = require('../server');
 const express = require('express');
 
-describe('index page', function () {
-    it('should show status 200', function (done) {
-        chai.request(app).get('/').end(function (err, res) {
-            res.should.have.status(200);
-            res.should.be.html;
-            done();
+const {
+    User,
+    Note
+} = require('../models');
+
+const {
+    DATABASE_URL,
+    TEST_DATABASE_URL
+} = require('../config');
+
+chai.use(chaiHttp);
+
+const usermame = 'demo'
+
+//Define functions
+function generateUser() {
+    return {
+        firstName: faker.random.first_name(),
+        lastName: faker.random.last_name(),
+        email: faker.internet.email(),
+        username: username,
+        password: faker.lorem.words()
+    }
+}
+
+function generateNote() {
+    return {
+        title: faker.lorem.sentence(),
+        body: faker.lorem.paragraphs(),
+        type: 'public',
+        username: username
+    }
+}
+
+function seedNote() {
+    console.info('Seeding notes');
+    const notes = [];
+    for (let i = 0; i < 10; i++) {
+        notes.push(generateNote())
+    }
+    return Note.insertMany(notes)
+}
+
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDataBase();
+}
+
+
+
+describe('ataNote APIs', () => {
+    before(() => {
+        return runServer(TEST_DATABASE_URL)
+            .then(console.log('running server'))
+            .catch(err => console.log({
+                err
+            }));
+    });
+    beforeEach(() => {
+        return seeNote();
+    });
+    describe('GET users endpoint', () => {
+        it('should return all users in db', () => {
+            let res;
+            return chai.request(app)
+                .get('/users')
+                .then((_res) => {
+                    res = _res;
+                    res.should.have.status(200);
+                    res.body.should.have.length.of.at.least(1);
+                });
         });
+    });
+    afterEach(() => {
+        return tearDownDb();
+    });
+    after(() => {
+        return closeServer();
     });
 });
